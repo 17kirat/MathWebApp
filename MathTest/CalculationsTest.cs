d9adc3f371c09c5dbacb158bea1082c9b868027f
Jaskirat KAUR <19kirat@gmail.com>
2022-02-22 21:57:44 -0500

    Added other operations in MathTest like Subtarct, Multiply and Division.

--- MathTest/CalculationsTest.cs
+++ MathTest/CalculationsTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathLibrary;
 
 namespace MathTest
 {
@@ -11,6 +12,31 @@ namespace MathTest
             MathCalculations math = new MathCalculations();
             double result = math.Calculate(5, 6, "Add");
             Assert.AreEqual(5, result); 
+        }
+
+        [TestMethod]
+        public void TestSubtract()
+        {
+            MathCalculations math = new MathCalculations();
+            double result = math.Calculate(5, 3, "Subtract");
+
+           Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            MathCalculations math = new MathCalculations();
+            double result = math.Calculate(6, 6, "Multiply");
+            Assert.AreEqual(36, result);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            MathCalculations math = new MathCalculations();
+            double result = math.Calculate(6, 6, "Divide");
+            Assert.AreEqual(1, result);
         }
     }
 }
